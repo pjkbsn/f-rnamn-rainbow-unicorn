3b86474ef32b690747b618372a032af951211f61
pjkbsn <philip.jkbsn@gmail.com>
2024-01-16 13:40:39 +0100

    2klar

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var animals = ['apa', 'orangutang', 'gorilla', 'get', 'giraff'];
 console.log(animals);
+var randomShit = {
+    string: 'w0rd',
+    number: 27,
+    bool: false,
+};
+console.log(randomShit);
--- app.ts
+++ app.ts
@@ -1,3 +1,17 @@
 const animals: string[] = ['apa', 'orangutang', 'gorilla', 'get', 'giraff'];
 
 console.log(animals);
+
+type RandomShitType = {
+    string: string;
+    number: number;
+    bool: boolean;
+};
+
+const randomShit: RandomShitType = {
+    string: 'w0rd',
+    number: 27,
+    bool: false,
+};
+
+console.log(randomShit);
